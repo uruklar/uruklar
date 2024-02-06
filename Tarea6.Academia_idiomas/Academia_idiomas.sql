a3e1cb88d3bd17a7502833a8c02dbbaa953b46ac
Xavi <javier.m.crespo@gmail.com>
2024-02-06 11:34:35 +0100

    Academia idiomas sql

---
+++ Tarea6.Academia_idiomas/Academia_idiomas.sql
@@ -0,0 +1,28 @@
+CREATE TABLE PROFESORES(
+    Dni_p VARCHAR(9) PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellidos VARCHAR(50) NOT NULL,
+    Direccion VARCHAR(100) NOT NULL,
+    TituloAcademico VARCHAR(4) CHECK(TituloAcademico IN('LDO', 'DDO', 'CFGS')),
+    PrecioHora DECIMAL(5,2) CHECK(PrecioHora > 0)
+);
+CREATE TABLE CURSOS(
+    Codigo_curso INT PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    NumeroAlumnos INT CHECK(NumeroAlumnos>=10),
+    Dni_p VARCHAR(9),
+    FechaInicio DATE NOT NULL,
+    FechaFinal DATE CHECK(FechaInicio<FechaFinal),
+    NumHoras INT CHECK(NumHoras>=80),
+    FOREIGN KEY (Dni_p) REFERENCES PROFESORES(Dni_p)
+);
+CREATE TABLE ALUMNOS(
+    Dni_a VARCHAR(9) PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellidos VARCHAR(50) NOT NULL,
+    Direccion VARCHAR(100) NOT NULL,
+    FechaNacimiento DATE Not NULL,
+    Sexo CHAR(1) CHECK(Sexo IN('H' , 'M')),
+    Codigo_curso INT,
+    FOREIGN KEY (Codigo_curso) REFERENCES CURSOS(Codigo_curso)
+);
